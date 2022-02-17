a5f746326773b4bbabce87c174a9589511f9ad1c
Brenda Ruiz <20001610@alumnos.utleon.edu.mx>
2022-02-17 16:01:21 -0600

    Esta es mi primer versión

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+/**
+ *
+ * @author brenr
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
